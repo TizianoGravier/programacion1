fd975af235fd8e94b261882aad79931c63f4ae9c
TizianoGravier <tgravier@udesa.edu.ar>
2022-07-01 14:33:19 -0300

    detalle artistas

---
+++ js/detalleartistas.js
@@ -0,0 +1,26 @@
+let queryString = location.search;
+let qsToObject = new URLSearchParams (queryString);
+let idArtistas = qsToObject.get ('id'); 
+
+console.log(queryString,'---', qsToObject,'---', idArtistas)
+
+fetch('https://api.allorigins.win/raw?url=https://api.deezer.com/artist/'+idArtistas)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data)
+
+    let nombreArtista = document.querySelector('.titulo')
+    nombreArtista.innerHTML = `<h2 class="titulo">${data.name}</h2>`
+
+    let fotoArtista = document.querySelector('.foto')
+    fotoArtista.innerHTML = `<article class="foto">
+    <img src=${data.picture} alt="foton de Coldplay">
+    </article>`
+
+    let tracklist = document.querySelector('.lista')
+    tracklist.innerHTML = `<ol class="lista">
+    <li>${data.tracklist}</li>
+    </ol>`
+})
