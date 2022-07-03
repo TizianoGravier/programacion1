c224237bcd317af517d2bcbe65f93b6c7ff43b90
TizianoGravier <tgravier@udesa.edu.ar>
2022-07-03 20:42:15 -0300

    agregado los buscadores

--- js/detallegeneros.js
+++ js/detallegeneros.js
@@ -1,3 +1,23 @@
+//buscador
+let buscador = document.querySelector('form')
+let search = document.querySelector(".search")
+
+buscador.addEventListener('submit',function(buscar){
+
+    buscar.preventDefault()
+    //console.log('error')
+
+    if(search.value == ''){
+        alert('que quiere buscar?')
+    }
+    else if(search.value.length < 3){
+        alert('escriba minimo 3 caracteres')
+    }
+    else{
+        this.submit()
+    }
+})
+
 let queryString = location.search;
 let qsToObject = new URLSearchParams (queryString);
 let idGeneros = qsToObject.get ('id'); 
--- js/index.js
+++ js/index.js
@@ -1,3 +1,25 @@
+//buscador
+let buscador = document.querySelector('form')
+let search = document.querySelector(".search")
+
+buscador.addEventListener('submit',function(buscar){
+
+    buscar.preventDefault()
+    //console.log('error')
+
+    if(search.value == ''){
+        alert('que quiere buscar?')
+    }
+    else if(search.value.length < 3){
+        alert('escriba minimo 3 caracteres')
+    }
+    else{
+        this.submit()
+    }
+})
+
+
+
 //canciones
 
 fetch('https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/tracks')
