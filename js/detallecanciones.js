40f5b425f5370943e9c2eb64fad02c0252e318ac
TizianoGravier <tgravier@udesa.edu.ar>
2022-07-04 01:47:37 -0300

    detalle de cancion para favoritos

--- js/detallecanciones.js
+++ js/detallecanciones.js
@@ -19,9 +19,9 @@ buscador.addEventListener('submit',function(buscar){
 })
 
 
-let queryString = location.search;
-let qsToObject = new URLSearchParams (queryString);
-let idCancion = qsToObject.get ('id'); 
+let queryString = location.search; //obtengo la qs
+let qsToObject = new URLSearchParams (queryString); //objeto literal basado en la qs
+let idCancion = qsToObject.get ('id'); //obtengo el id de la canción
 
 console.log(queryString,'---', qsToObject,'---', idCancion)
 
@@ -60,7 +60,46 @@ fetch('https://api.allorigins.win/raw?url=https://api.deezer.com/track/'+idCanci
         <iframe title="deezer-widget" src="https://widget.deezer.com/widget/dark/track/${data.id}" width="400" height="300" frameborder="0" allowtransparency="true" allow="encrypted-media; clipboard-write"></iframe>
     </article> `
 
-    
 
+    //guardar en favoritos
+    let favoritos = [ ]
+
+    //chequeo si hay algo en favoritos
+    let recuperoStorage = localStorage.getItem('favoritos')
+    console.log(recuperoStorage)
+
+    if(recuperoStorage){ ////null o undefined => false //tiene una variable => true
+        let favoritosArray = JSON.parse(recuperoStorage)
+        favoritos = favoritosArray
+        
+    }
+
+    let linkFav = document.querySelector('.agregarFav')
+
+    if(favoritos.includes(idCancion)){ //para que cambie el texto si la cancion ya esta en favoritos
+        linkFav.innerHTML = "Sacar de favoritos"
+    }
+
+    //definir un evento para el link a favoritos
+    linkFav.addEventListener('click',function(evento){
+        evento.preventDefault()
+
+        if(favoritos.includes(idCancion)){ //si ya esta en el array, lo saco y cambio el texto
+            let sacarCancion = favoritos.indexOf(idCancion)
+            favoritos.splice(sacarCancion,1)
+            linkFav.innerHTML = "Agregar a favoritos"
+
+        }else{ //si no esta en el array, lo agrega y cambio el texto
+            favoritos.push(idCancion)
+            linkFav.innerHTML = "Sacar de favoritos"
+        }
+
+        //Pasar el array a string para agregarlo al localStorage
+        let cancionesFavoritasString = JSON.stringify(favoritos)
+        localStorage.setItem('favoritos', cancionesFavoritasString)
+
+        console.log(localStorage.getItem('favoritos'))
+
+        
+    })
 })
-
